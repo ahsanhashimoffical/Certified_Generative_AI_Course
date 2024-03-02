3f1c1bc82eecb500f097894280c1d1367919df3c
ahsanhashimoffical <161936575+ahsanhashimoffical@users.noreply.github.com>
2024-03-02 15:40:21 +0500

    Add files via upload

---
+++ Names.ts
@@ -0,0 +1,5 @@
+let members: string[] = ["Alice", "Bob", "Charlie", "David", "Eve"];
+let massage: string = "Invation of AI Course - ";
+for (let i = 0; i < members.length; i++) {
+    console.log(massage + members[i]);
+}
---
+++ names.js
@@ -0,0 +1,5 @@
+var members = ["Alice", "Bob", "Charlie", "David", "Eve"];
+var massage = "Invation of AI Course - ";
+for (var i = 0; i < members.length; i++) {
+    console.log(massage + members[i]);
+}
